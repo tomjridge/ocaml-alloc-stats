5c056a7ab0550094de6e5c97d79c65f74c7d1d92
Tom Ridge <tom.j.ridge@googlemail.com>
2022-11-23 15:26:58 +0000

    Add timestamps back into dump traces

--- bin/dump_trace.ml
+++ bin/dump_trace.ml
@@ -1,17 +1,17 @@
 (* NOTE based on memtrace's bin/dump_trace 
 
 Typical output:
-0000000000 A 376181
-0000000001 a 4
-0000000001 P
-0000000002 a 12
-0000000002 C
+0000013924 0000000000 A 376181
+0000026254 0000000001 a 4
+0000027511 0000000001 P
+0000028849 0000000002 a 12
 
 Output consists of lines with the following format:
 
-id (A|a|AE|P|C) len?
+timestamp id (A|a|AE|P|C) len?
 
 where:
+- timestamp is the timestamp of the event
 - id is the id of the event
 - the type of the allocation is: A (major), a (minor), AE (external), P (promotion), C (collect)
 - len is the length, when the event is an allocation, otherwise there is no len
@@ -22,8 +22,8 @@ open Memtrace.Trace
 let dump filename outfile =
   let trace = Reader.open_ ~filename in
   let outch = Stdlib.open_out outfile in
-  Reader.iter trace (fun _time ev ->
-    (* Printf.printf "%010Ld " (Timedelta.to_int64 time); *)
+  Reader.iter trace (fun time ev ->
+    (* Printf.fprintf "%010Ld " (Timedelta.to_int64 time); *)
     match ev with
   | Alloc {obj_id; length; nsamples; source; backtrace_buffer; backtrace_length; common_prefix} ->
     let src =
@@ -31,7 +31,7 @@ let dump filename outfile =
       | Minor -> "a"
       | Major -> "A"
       | External -> "AE" in
-    Printf.fprintf outch "%010d %s %d" (obj_id :> int) src length;
+    Printf.fprintf outch "%010Ld %010d %s %d" (Timedelta.to_int64 time) (obj_id :> int) src length;
     ignore(backtrace_buffer,backtrace_length,common_prefix,nsamples);
     (*
     let print_location ppf loc =
@@ -45,9 +45,9 @@ let dump filename outfile =
        *)
     Printf.fprintf outch "\n%!"
   | Promote id ->
-    Printf.fprintf outch "%010d P\n" (id :> int)
+    Printf.fprintf outch "%010Ld %010d P\n" (Timedelta.to_int64 time) (id :> int)
   | Collect id ->
-    Printf.fprintf outch "%010d C\n" (id :> int));
+    Printf.fprintf outch "%010Ld %010d C\n" (Timedelta.to_int64 time) (id :> int));
   Stdlib.close_out outch;
   Reader.close trace
 
